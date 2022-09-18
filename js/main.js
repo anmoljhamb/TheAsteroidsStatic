1471677987624f937f5412fd03716d0fd54748be
Anmol Jhamb <talktoanmol@outlook.com>
2022-09-19 00:06:19 +0530

    merged temp to index.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-window.onscroll = function() {
-    onScrollFunction();
-}
+function onReady() {
+    let text = document.getElementById("text");
+    let bird1 = document.getElementById("bird1");
+    let bird2 = document.getElementById("bird2");
+    let btn = document.getElementById("btn");
+    let stones = document.getElementById("stones");
+    let forest = document.getElementById("forest");
+    let water = document.getElementById("water");
+    let header = document.getElementById("header");
 
+    console.log("default values: ");
+    console.log("text.style.top" + text.style.top)
 
-function onScrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementsByClassName("navbar")[0].style.backgroundColor = "white";
-    } else {
-        document.getElementsByClassName("navbar")[0].style.backgroundColor = "transparent";
-    }
+    window.addEventListener('scroll', function() {
+
+        let value = window.scrollY;
+
+        console.log("value: " + value)
+
+        if (value <= 10) {
+            text.style.top = '';
+        } else {
+            text.style.top = 50 + value * -.1 + '%';
+            bird2.style.top = value * -1.5 + 'px';
+            bird2.style.left = value * 2 + 'px';
+            bird1.style.top = value * -1.5 + 'px';
+            bird1.style.left = value * -5 + 'px';
+            btn.style.marginTop = value * 1.5 + 'px';
+            stones.style.top = value * -.12 + 'px';
+            forest.style.top = value * .25 + 'px';
+            header.style.top = value * .5 + 'px';
+        }
+    })
 }
