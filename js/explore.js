2775b72de29a8240de7f6dae4f7db1e44d4d61d3
Anmol Jhamb <talktoanmol@outlook.com>
2022-09-19 19:31:16 +0530

    Final animation done

--- js/explore.js
+++ js/explore.js
@@ -1,14 +1,19 @@
 distance = 0;
 
+function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function random(arr) {
-    return arr[Math.random() * arr.length | 0];
+    return arr[randomNumber(0, arr.length)];
 }
 
 
-function move(i) {
+function move(i, d = 200) {
 
-    distance = distance + 200 * i
+    distance = distance + d * i
 
     text = document.querySelector(".text");
     video = document.querySelector(".video");
@@ -20,40 +25,67 @@ function move(i) {
 
 
 
-function onReady() {
 
+function onReady() {
+    const mouseoverEvent = new Event('mouseover');
     mini_title = document.querySelector(".mini-title")
     big_title = document.querySelector(".big-title strong")
     quote_text = document.querySelector(".quote-text")
     video = document.querySelector(".landing-page .video")
     bgImage = document.querySelector(".background-landing-page")
+    left = document.getElementById("left");
+    right = document.getElementById("right");
+
+    function setTemplate(place) {
+        text = document.querySelector(".text");
+        video = document.querySelector(".video");
+
+        text.style.transform = "translateX(0px)"
+        video.style.transform = "translateX(0px)"
+        mini_title.innerHTML = place.place_name
+        big_title.innerHTML = place.place_name
+        if (quote_text.length)
+            quote_text.innerHTML = place.quote_text
+        else
+            quote_text.innerHTML = ""
+        video.style.backgroundImage = `url("${place.image_link}")`
+        bgImage.style.backgroundImage = `url("${place.image_link}")`
+        document.querySelector('.btn').addEventListener('click', () => {
+            window.open(place.wiki_link);
+        })
+    }
+
 
     fetch("/assets/data.json")
         .then((response) => response.json())
         .then((data) => {
-            place = random(data.places);
-            mini_title.innerHTML = place.place_name
-            big_title.innerHTML = place.place_name
-            if (quote_text.length)
-                quote_text.innerHTML = place.quote_text
-            else
-                quote_text.innerHTML = ""
-            video.style.backgroundImage = `url("${place.image_link}")`
-            bgImage.style.backgroundImage = `url("${place.image_link}")`
-            document.querySelector('.btn').addEventListener('click', () => {
-                window.open(place.wiki_link);
+            arr = data.places
+            curr_index = randomNumber(0, arr.length - 1);
+            console.log("curr_index: " + curr_index)
+            place = arr[curr_index];
+            setTemplate(place)
+            right.addEventListener('click', () => {
+                curr_index = curr_index + 1 + arr.length;
+                curr_index = curr_index % arr.length;
+                place = arr[curr_index]
+                setTemplate(place)
+            })
+
+            left.addEventListener('click', () => {
+                curr_index = curr_index - 1 + arr.length;
+                curr_index = curr_index % arr.length;
+                place = arr[curr_index]
+                setTemplate(place)
+                console.log("left click")
             })
         });
+
 
     text = document.querySelector(".text");
     video = document.querySelector(".video");
 
     text.style.transform = "translateY(" + distance + "px)"
     video.style.transform = "translateY(" + distance + "px)"
-
-
-    left = document.getElementById("left");
-    right = document.getElementById("right");
 
     left.addEventListener('mouseover', function() {
         console.log("left supremacy")
